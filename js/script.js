dfd3259e93207d0b9b2e92c863a7753dfc075db1
imartinez0753 <50923315+imartinez0753@users.noreply.github.com>
2020-06-17 17:48:06 -0700

    I'm gaining on it

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ $("#poop").on("click", function () {
 	console.log("hit");
 	var cityName = $("#cityInput").val();
 	var cityResult = $("<button>");
-
+	// console.log(tempature);
 	cityResult.addClass("list-group-item");
 	cityResult.text(cityName);
 	$("#cityUl").append(cityResult);
@@ -21,8 +21,52 @@ $("#poop").on("click", function () {
 	}).then(function (response) {
 		console.log(response);
 		var tempature = (response.main.temp * 1.8 - 459.67).toFixed(2);
+		var humidity = response.main.humidity;
+		var windSpeed = response.wind.speed;
+
+		var lonEl = response.coord.lon;
+		var latEl = response.coord.lat;
+		// lon.push(lonEl);
+		// lat.push(latEl);
 		console.log(tempature);
 		$("#tempature").text("Tempature: " + tempature);
+		$("#humidity").text("Humidity: " + humidity);
+		$("#windSpeed").text("Wind speed is: " + windSpeed);
+		// $("#uvIndex").text("Uv Index: " + uvIndex);
+		var uvIndex =
+			"http://api.openweathermap.org/data/2.5/uvi?appid=6f57cdea4243d1500ab921ec9947fe41&lat=" +
+			latEl +
+			"&lon=" +
+			lonEl;
+		$.ajax({
+			url: uvIndex,
+			method: "GET",
+		}).then(function (data) {
+			console.log(data);
+			var uvIndex = data.value;
+			$("#uvIndex").text("Uv Index: " + uvIndex);
+		});
+
+		var forecast =
+			"api.openweathermap.org/data/2.5/forecast?q={city name}&appid={your api key}" +
+			"api.openweathermap.org/data/2.5/forecast?id={city ID}&appid={your api key}";
+		// console.log(cityName);
+		$.ajax({
+			url:
+				"https://api.openweathermap.org/data/2.5/forecast?q=" +
+				cityName +
+				",us&appid=6f57cdea4243d1500ab921ec9947fe41",
+			method: "GET",
+		}).then(function (forecast) {
+			console.log(forecast);
+		});
 	});
-	console.log($("#cityInput").val());
+	// var lon = [];
+	// var lat = [];
+	// var lonStr = lon[0];
+	// var latStr = lat[0];
+	// console.log(lonStr);
+	// console.log(latStr);
+
+	// console.log($("#cityInput").val());
 });
